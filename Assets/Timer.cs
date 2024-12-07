e6503c191039dae228ab58ceafc085e5b07f7233
Brian Zheng <brianzheng205@gmail.com>
2024-12-07 14:19:47 -0500

    Create losing text

--- Assets/Timer.cs
+++ Assets/Timer.cs
@@ -7,25 +7,31 @@ public class Timer : MonoBehaviour
 {
     public GameObject pineappleScript;
 
-    private PlayerController player;
+    private PlayerController playerController;
+    private Health playerHealth;
 
     // Start is called before the first frame update
     void Start()
     {
-        player = transform.parent.parent.GetComponent<PlayerController>();
+        playerController = transform.parent.parent.GetComponent<PlayerController>();
+        playerHealth = transform.parent.parent.GetComponent<Health>();
         GetComponent<TextMeshProUGUI>().text = "Time left: " + pineappleScript.GetComponent<PineappleScript>().SecUntilPineappleFinish.ToString("F1");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (pineappleScript.GetComponent<PineappleScript>().SecUntilPineappleFinish <= 0f)
+        if (playerHealth.currentHealth <= 0)
+        {
+            GetComponent<TextMeshProUGUI>().text = "You Lose!";
+        }
+        else if (pineappleScript.GetComponent<PineappleScript>().SecUntilPineappleFinish <= 0f)
         {
             GetComponent<TextMeshProUGUI>().text = "You Win!";
-            player.canMove = false;
-            player.canLook = false;
-            player.canFire = false;
-            player.canInteract = false;
+            playerController.canMove = false;
+            playerController.canLook = false;
+            playerController.canFire = false;
+            playerController.canInteract = false;
         }
         else
         {
