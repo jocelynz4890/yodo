fd502b0c3d96b1058423e1bc657d668c95707b98
Z-Prewitt <eminor7@mit.edu>
2024-12-06 22:25:04 -0500

    Title and menu update

--- Assets/MenuManager.cs
+++ Assets/MenuManager.cs
@@ -5,6 +5,7 @@ public class MenuManager : MonoBehaviour
 {
     public GameObject mainMenuUI;
     public GameObject creditsUI;
+    public GameObject instructionsUI;  // Added instructions UI reference
 
     public void ShowCredits()
     {
@@ -12,9 +13,16 @@ public class MenuManager : MonoBehaviour
         creditsUI.SetActive(true);
     }
 
+    public void ShowInstructions()  // New method to show instructions
+    {
+        mainMenuUI.SetActive(false);
+        instructionsUI.SetActive(true);
+    }
+
     public void BackToMenu()
     {
         creditsUI.SetActive(false);
+        instructionsUI.SetActive(false);  // Added to handle instructions panel
         mainMenuUI.SetActive(true);
     }
 }
